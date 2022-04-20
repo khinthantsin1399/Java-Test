8ef31ace1eb8150950bc72d6e8761b11de4e65f6
khinthantsin <khinthantsin@localhost>
2022-04-20 15:41:22 +0630

    test commit

---
+++ java project/src/eg1.java
@@ -0,0 +1,13 @@
+
+public class eg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;//10
+		int i;
+		i=a<<1;//100
+		System.out.println("the value of a before left shift is: " +a);
+		System.out.println("the value of a after applying left shift is: " +i);
+	}
+
+}
