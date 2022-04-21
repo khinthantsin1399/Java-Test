86017db00c0b4a1919fdf606f17e57dcd192faf8
Khin Thant Sin <khinthantsin1399@gmail.com>
2022-04-21 10:42:12 +0630

    Day3 Modified Commit

---
+++ TestJava/src/ojt/test/ForLoop.java
@@ -0,0 +1,29 @@
+package ojt.test;
+
+/**
+ * <h2>ForLoop Class</h2>
+ * <p>
+ * Process for Displaying ForLoop
+ * </p>
+ * 
+ * @author khinthantsin
+ */
+public class ForLoop {
+
+    /**
+     * <h2>main</h2>
+     * <p>
+     * Main Method
+     * </p>
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int x = 1; x <= 20; x++) {
+            sum = sum + x;
+        }
+        System.out.println("Sum: " + sum);
+    }
+
+}
