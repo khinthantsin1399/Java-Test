4901850e086d6fddab6e54f36a1c70931358bca3
Khin Thant Sin <khinthantsin1399@gmail.com>
2022-04-21 10:58:09 +0630

    nn

--- TestJava/src/ControlExample.java
+++ TestJava/src/ControlExample.java
@@ -1,43 +1,42 @@
 
 /**
- * <h2> ControlExample Class</h2>
+ * <h2>ControlExample Class</h2>
  * <p>
  * Process for Displaying ControlExample
  * </p>
+ * 
  * @author khinthantsin
  */
 public class ControlExample {
+    /**
+     * <h2>main</h2>
+     * <p>
+     * Main Method
+     * </p>
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        if (x > y) {
+            System.out.println("x is greater than y.");
+        } else if (x < y) {
+            System.out.println("x is less than y.");
+        } else {
+            System.out.println("x is equal to y.");
+        }
 
-	/**
-	 * <h2> main</h2>
-	 * <p>
-	 * Main Method
-	 * </p>
-	 * @param args
-	 */
-	public static void main(String[] args) {
-	int x=5;
-	int y=10;
-	if(x>y) {
-		System.out.println("x is greater than y.");
-	} else if(x<y) {
-		System.out.println("x is less than y.");
-	} else {
-		System.out.println("x is equal to y.");
-	}
-	
-	System.out.println("\nDo..While Loop");
-	do{   
-		
-        System.out.println(x);    
-    x++;    
-    }while(x<=10);  
-	
-	System.out.println("\nWhile Loop");
-	while(y<=15){  
-		
-        System.out.println(y);  
-    y++;  
-    }  
-}
+        System.out.println("\nDo..While Loop");
+        do {
+            System.out.println(x);
+            x++;
+        } while (x <= 10);
+
+        System.out.println("\nWhile Loop");
+        while (y <= 15) {
+            System.out.println(y);
+            y++;
+        }
+    }
 }
