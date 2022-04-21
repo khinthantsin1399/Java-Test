a7a0df4112e3858c0a7b0b8e290d1c08a6e78375
Khin Thant Sin <khinthantsin1399@gmail.com>
2022-04-21 10:02:31 +0630

    Day3 Commit

---
+++ TestJava/src/Student.java
@@ -0,0 +1,62 @@
+
+/**
+ * <h2> Student Class</h2>
+ * <p>
+ * Process for Displaying Student
+ * </p>
+ * @author khinthantsin
+ */
+public class Student {
+	String name;
+	int id;
+	
+	/**
+	 * <h2> Constructor for Student </h2>
+	 * <p>
+	 * Constructor for Student
+	 * </p>
+	 * @param name
+	 * @param id
+	 */
+	Student(String name, int id) {
+		this.name = name;
+		this.id =id;
+	}
+	
+	/**
+	 * <h2> display</h2>
+	 * <p>
+	 * Method for displaying student information
+	 * </p>
+	 * @param name
+	 * @param id
+	 */
+	public void display() {
+		System.out.println(name + " " + id);
+	}
+	
+	//Static Method
+	/**
+	 * <h2> displayAddress</h2>
+	 * <p>
+	 * Method for displaying address
+	 * </p>
+	 * @param address
+	 */
+	public static void displayAddress(String address) {
+		System.out.println(address);
+	}
+	
+	/**
+	 * <h2> main</h2>
+	 * <p>
+	 * Main method
+	 * </p>
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Student student = new Student("khin",3);//Object Creation
+		student.display();
+		Student.displayAddress("Yangon");
+	}
+}
---
+++ TestJava/src/SwitchExample.java
@@ -0,0 +1,53 @@
+
+/**
+ * <h2> SwitchExample Class</h2>
+ * <p>
+ * Process for Displaying SwitchExample
+ * </p>
+ * @author khinthantsin
+ */
+public class SwitchExample {
+	enum Days {
+	    MON,
+	    TUE,
+	    WED,
+	    THUR,
+	    FRI
+	}
+	/**
+	 * <h2> main</h2>
+	 * <p>
+	 * main method
+	 * </p>
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		for (Days days : Days.values()) { 
+			if(days==Days.THUR) {
+				continue;
+			}
+		    System.out.println(days); 
+		}
+		Days day;
+		day = Days.THUR;
+		switch (day) {
+        case MON:
+            System.out.println("\nToday is Monday.");
+            break;
+        case TUE:
+            System.out.println("\nToday is Tuesday.");
+            break;
+        case WED:
+            System.out.println("\nToday is Wednesday.");
+            break;
+        case THUR:
+            System.out.println("\nToday is Thursday.");
+            break;
+        case FRI:
+            System.out.println("\nToday is Friday.");
+            break;
+    }	
+		
+}
+}
